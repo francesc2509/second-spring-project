f83b7cd63b3973695d74e02675d29a35f157f872
Francesc Manarel Bueno <francesc2509@outlook.com>
2021-03-24 23:13:47 +0000

    Add Autowired to class attribute

--- src/com/springcourse/annotations/model/SalesManager.java
+++ src/com/springcourse/annotations/model/SalesManager.java
@@ -5,16 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component("salesManager")
 public class SalesManager implements Employee {
-	
+	@Autowired
 	private FinancialReportBuilder financialReportBuilder;
 
-	public SalesManager() {}
-	
-	@Autowired
-	public SalesManager(FinancialReportBuilder financialReportBuilder) {
-		this.financialReportBuilder = financialReportBuilder;
-	}
-	
 	@Override
 	public String getTasks() {
 		return "Manage company's products";
